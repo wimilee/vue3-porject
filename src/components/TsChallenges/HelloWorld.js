4e7c77a9427d9a8bb7bb93f8be40b0a54fb34b40
wimilee <wimilee@163.com>
2022-02-27 20:56:38 +0800

    test

---
+++ src/components/TsChallenges/HelloWorld.js
@@ -0,0 +1,6 @@
+const hw = '3';
+const apple = 4;
+console.log(hw);
+console.log(apple);
+export {};
+//# sourceMappingURL=HelloWorld.js.map
---
+++ src/components/TsChallenges/HelloWorld.ts
@@ -0,0 +1,10 @@
+type HelloWorld = string // expected to be a string
+//type HelloWorld = any
+/* _____________ 测试用例 _____________ */
+import { Equal, Expect, NotAny } from '@type-challenges/utils'
+
+type cases = [Expect<NotAny<HelloWorld>>, Expect<Equal<HelloWorld, string>>]
+const hw: HelloWorld = '3'
+const apple: HelloWorld = 4
+console.log(hw)
+console.log(apple)
