5cac29351fe503ede7aaeb3e7d717a7a2b04c7bb
wimilee <wimilee@163.com>
2022-03-01 22:06:54 +0800

    base

---
+++ src/components/TsChallenges/function.ts
@@ -0,0 +1,11 @@
+//参数类型定义
+function dec(a: number, b: number) {
+  return a - b
+}
+console.log(dec(2, 3))
+
+function avg(a: number, b: number, avge?: number) {
+  return a / b
+}
+
+console.log(avg)
--- src/components/TsChallenges/type.ts
+++ src/components/TsChallenges/type.ts
@@ -24,5 +24,45 @@ let score: string | number = '不及格'
 console.log(score)
 score = 59
 console.log(score)
+//let iphone: [string | number | boolean] = [9999, '苹果', 31, true]
+//元祖有元素顺序和容量固定
 let iphone: (string | number | boolean)[] = [9999, '苹果', 31, true]
 console.log(iphone)
+//
+let thing: any
+thing = 'apple'
+console.log(typeof thing + thing)
+thing = 3
+console.log(typeof thing + thing)
+thing = false
+console.log(typeof thing + thing)
+thing = ['hw', 'xiaomi']
+console.log(typeof thing + thing)
+thing = ['jack', 18, false]
+console.log(typeof thing + thing)
+thing = { name: 'rose', age: 24 }
+console.log(typeof thing + thing)
+thing = [9999, '苹果', 31, true]
+console.log(typeof thing + thing)
+thing = class ponit {
+  constructor() {}
+  play() {
+    console.log('是个对象')
+  }
+}
+console.log(typeof thing + thing)
+
+class ab {
+  constructor() {}
+  get = () => 'baby'
+}
+
+let anglebaby: any = new ab()
+console.log(anglebaby.get())
+
+//unknown 需要明确类型后赋值
+
+let hd: unknown = 'howdo'
+//let a:string = hd  不能将类型unknown 分配给string
+let b: string = hd as string
+console.log(b)
